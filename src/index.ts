3e6fd8ba7115bc400f41c94a51b0877c994176c9
django <six.django@gmail.com>
2023-04-27 11:07:05 +0800

    add a line command "-v or -version" for check current version

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@
 import inquirer from 'inquirer';
 import { ProjectTemplate } from './enum';
 import { generateProject } from './projectGenerator';
+import pkg from '../package.json'
 
 
+const version = pkg.version;
+
+// 查看版本号
+if (process.argv.includes('-v') || process.argv.includes('--version')) {
+  console.log(version);
+  process.exit(0);
+}
 
 async function main() {
   const { projectChoice } = await inquirer.prompt([
@@ -13,7 +21,8 @@ async function main() {
       message: '请选择一个项目模板:',
       choices: [
         ProjectTemplate.Vue3,
-        ProjectTemplate.Nuxt
+        ProjectTemplate.Nuxt,
+        ProjectTemplate.Express
         // 添加更多项目模板选项
       ],
     },
